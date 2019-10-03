5f582cb5556003b5bf28b88f60d3e02ccadab96e
rpena <rodrigo.pena@columbia.com>
2019-10-03 13:09:44 -0700

    added scss and bootstrap, included toolbar in main view

--- src/app/components/pdp/pdp.component.ts
+++ src/app/components/pdp/pdp.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pdp',
   templateUrl: './pdp.component.html',
-  styleUrls: ['./pdp.component.css']
+  styleUrls: ['./pdp.component.scss']
 })
 export class PdpComponent implements OnInit {
 
